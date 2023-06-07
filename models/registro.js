5998afa6c889bb2f9651265a9c3204967516704d
devruso <jamilsonjr@live.com>
2023-06-07 13:39:31 -0300

    model registration

---
+++ models/registro.js
@@ -0,0 +1,30 @@
+const { DataTypes, UUID } = require("sequelize");
+const { connection } = require("../database/database");
+const Usuario = require("./usuario");
+
+const Registro = connection.define('usuario', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    DataHora_Chegada: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    DataHora_Saida: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        unique: true
+    },
+});
+
+Vaga.hasMany(Registro, {
+    onDelete: "CASCADE"
+  });
+// Faltando implementar o model de avaliacao
+Registro.belongsTo(Avaliacao);
+Registro.belongsTo(Usuario);
+Usuario.hasMany(Registro, {onDelete: "CASCADE"});
+module.exports = Usuario;
